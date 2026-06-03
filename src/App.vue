2574ab98a0879fa72061d72b22a5343eaa72a492
ZCDC_Ren <renzw@zzes1314.cn>
2026-06-03 16:58:14 +0800

    fix: pnpm workspace

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <el-config-provider :locale="lang">
-    <div class="flex w-screen h-screen">
-      <div class="w-3xs h-full"></div>
-      <ImageCanvas class="flex-1 h-full" />
-    </div>
-  </el-config-provider>
+    <el-config-provider :locale="lang">
+        <div class="flex w-screen h-screen">
+            <ImageCanvas class="flex-1 h-full" />
+        </div>
+    </el-config-provider>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-import ImageCanvas from './components/ImageCanvas.vue'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { ref } from "vue";
+import ImageCanvas from "./components/ImageCanvas.vue";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
-const lang = ref(zhCn)
+const lang = ref(zhCn);
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
